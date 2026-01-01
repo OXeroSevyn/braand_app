fec20dca0a4049c1e1682541f41dc83d1c866a93
OXeroSevyn <subhamdey.one@gmail.com>
2026-01-01 23:21:08 +0530

    Built web
    Branch: master
    Commit: b2f721ea67062a1ea20e09dbc7b15f8c44b3866f (dirty)
    
    package:peanut 6.0.0

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3834950821"
+    serviceWorkerVersion: "1248160253"
   }
 });
