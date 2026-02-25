96ee88be4611fa5b005f0d933ec41d7698757f81
OXeroSevyn <subhamdey.one@gmail.com>
2026-02-25 16:03:33 +0530

    Add vercel-build.sh for automatic Flutter build on Vercel

---
+++ vercel-build.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# 1. Download Flutter SDK
+if [ ! -d "flutter" ]; then
+  git clone https://github.com/flutter/flutter.git -b stable
+fi
+
+# 2. Add to PATH
+export PATH="$PATH:$(pwd)/flutter/bin"
+
+# 3. Pre-cache and check
+flutter precache
+flutter doctor
+
+# 4. Build
+flutter build web --release --base-href=/
