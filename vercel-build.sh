dc91f2dee43881db3a9dbe4aa7d42db74307d357
OXeroSevyn <subhamdey.one@gmail.com>
2026-02-25 17:31:09 +0530

    Fix Vercel build script: remove invalid flag and improve environment setup

--- vercel-build.sh
+++ vercel-build.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-# 1. Download Flutter SDK
+# 1. Download Flutter SDK if not present
 if [ ! -d "flutter" ]; then
   git clone https://github.com/flutter/flutter.git -b stable
 fi
 
-# 2. Add to PATH
-export PATH="$PATH:$(pwd)/flutter/bin"
+# 2. Prepend to PATH to ensure we use our localized version
+export PATH="$(pwd)/flutter/bin:$PATH"
 
-# 3. Pre-cache and check
-flutter precache
-flutter doctor
+# 3. Configure and Prepare
+flutter config --enable-web
+flutter precache --web
+flutter pub get
 
-# 4. Build
-flutter build web --release --web-renderer html --base-href=/
+# 4. Build 
+# Note: Removed --web-renderer as it's not recognized by this version's CLI
+flutter build web --release --base-href=/
